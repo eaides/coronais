1dd56a3f67e24e672ce98cdd4bf7805e48b418c9
vagrant <vagrant@homestead>
2020-06-11 07:28:38 +0000

    update composer and npm

--- public/js/app.js
+++ public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ernesto/Code/Laravel/coronais/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/ernesto/Code/Laravel/coronais/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/coronais/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/coronais/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
