454e4297082e0225aa327e0ac56b4c07c6b093e4
vagrant <vagrant@homestead>
2021-01-20 07:02:26 +0000

    update dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ernesto/Code/Laravel/coronais/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/ernesto/Code/Laravel/coronais/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/coronais/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/coronais/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
